e7d50d0d26784d92df6c5df1c0ab0503132ef614
thanh727242 <thanh727242@gmail.com>
2024-11-26 23:08:10 +0700

    them_Auth

---
+++ Back_End/middlewares/authenticateToken.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'Access Denied' });
+
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid Token' });
+        req.user = user; // Lưu thông tin người dùng từ token
+        next();
+    });
+};
+
+module.exports = authenticateToken;
