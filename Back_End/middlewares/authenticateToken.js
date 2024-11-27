f8dfcb9db6b22f32cea176e7b48aad49a6e3f3d6
thanh727242 <thanh727242@gmail.com>
2024-11-27 13:51:05 +0700

    minifix_Auth

--- Back_End/middlewares/authenticateToken.js
+++ Back_End/middlewares/authenticateToken.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const authenticateToken = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
